3c321c23ee95de0e49281a18c63476e0903a0255
Nick Ilichev <spcazaam@yandex.ru>
2020-05-12 01:27:27 +0300

    create app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TodoList from './Todo/TodoList'
+
+function App() {
+  return (
+      <div className='wrapper'>
+        <h1>ReactJS Edu</h1>
+      
+      </div>
+    )
+}
+
+export default App;
